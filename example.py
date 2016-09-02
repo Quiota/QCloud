c4906198ea74fc4221570736a54af42dbf2d8649
quiota-leightondong <leighton.dong@quiota.com>
2016-09-01 18:24:19 -0700

    initial commit

---
+++ example.py
@@ -0,0 +1,47 @@
+from s3_operator import S3Operator
+
+# input your credentials
+key_id = ''
+key = ''
+
+# declare obejct
+s3 = S3Operator(key_id, key)
+s3.creat_client()
+
+# select bucket
+
+select_bucket = 'test.tf.quiota.com'
+
+# get filenames
+s3.get_filenames(select_bucket)
+
+# print filenames
+for filename in s3.filenames:
+	print filename
+
+# upload single file
+local_filename = './test.txt'
+cloud_filename = 'test/test/test.txt'
+
+s3.upload_single_file(local_filename, select_bucket, cloud_filename, 
+    cloud_storage_class="STANDARD")
+
+# download single file2
+local_filename = './test/test1/test.txt'
+cloud_filename = 'test/test/test.txt'
+s3.download_single_file(local_filename, select_bucket, cloud_filename)
+
+# upload multiple files
+local_path = './test/'
+cloud_folder_name = 'test'
+
+s3.upload_multiple_files(local_path, select_bucket, 
+    cloud_folder_name, cloud_storage_class="STANDARD")
+
+# download multiple files
+s3.get_filenames(select_bucket)
+conditions = ['/test1/']
+select_files = [f for f in s3.filenames if any(condition in f for condition in conditions)]
+
+local_path = './'
+s3.download_multiple_files(select_files, local_path, select_bucket)
---
+++ s3_operator.py
@@ -0,0 +1,101 @@
+import boto3
+import os
+
+
+class S3Operator(object):
+
+    def __init__(self, aws_access_key_id, aws_secret_access_key):
+        self.aws_access_key_id = aws_access_key_id
+        self.aws_secret_access_key = aws_secret_access_key
+
+    def creat_client(self):
+        # initialize client
+        self.client = boto3.client('s3', 
+            aws_access_key_id=self.aws_access_key_id,
+            aws_secret_access_key=self.aws_secret_access_key)
+
+    def get_filenames(self, select_bucket):
+        # Create a reusable Paginator
+        paginator = self.client.get_paginator('list_objects')
+        # Create a PageIterator from the Paginator
+        page_iterator = paginator.paginate(Bucket=select_bucket)
+        # list all files and folder/files
+        self.filenames = [content['Key'] for page in page_iterator 
+            for content in page['Contents']]
+
+    def upload_single_file(self, local_filename, select_bucket, cloud_filename,
+        cloud_storage_class="STANDARD"):
+        
+        print 'uploading to bucket {0}'.format(select_bucket)
+        print 'uploading to filename {0}'.format(cloud_filename)
+        print 'uploading from {0}'.format(local_filename)
+        
+        # use STANDARD_IA for infrequent access files
+        self.client.upload_file(local_filename, select_bucket, cloud_filename, 
+            ExtraArgs={'StorageClass':cloud_storage_class})
+    
+    def download_single_file(self, local_filename, select_bucket, 
+        cloud_filename):
+        
+        print 'downloading to bucket {0}'.format(select_bucket)
+        print 'downloading from filename {0}'.format(cloud_filename)
+        print 'downloading to {0}'.format(local_filename)
+
+        local_path = os.path.join(*(local_filename.split('/')[:-1]))
+        try:
+            os.makedirs(local_path)
+        except OSError as e:
+            pass 
+
+        self.client.download_file(select_bucket, cloud_filename, 
+            local_filename)
+
+    def upload_multiple_files(self, local_path, select_bucket, 
+        cloud_folder_name, cloud_storage_class="STANDARD"):
+        # upload multiple files
+        # you need to decide what files to be uploaded
+        select_files = []
+        for root, dirs, filenames in os.walk(local_path):
+            for filename in filenames:
+              local_file_path = os.path.join(root, filename)
+              select_files.append(local_file_path)
+
+        for local_filename in select_files:
+            cloud_filename = '{0}/{1}'.format(cloud_folder_name, 
+                local_filename.replace(local_path, ''))
+            print 'uploading to bucket {0}'.format(select_bucket)
+            print 'uploading to filename {0}'.format(cloud_filename)
+            print 'uploading from {0}'.format(local_filename)
+            
+            # use STANDARD_IA for infrequent access files
+            self.client.upload_file(local_filename, select_bucket, 
+                cloud_filename, ExtraArgs={'StorageClass':cloud_storage_class})
+        
+    def download_multiple_files(self, select_files, local_path, select_bucket):
+        # download multiple files
+        # you need to decide what files to be downloaded
+        for cloud_filename in select_files:
+            
+            cloud_path_list = cloud_filename.split('/')
+            local_file_path = os.path.join(local_path, *(cloud_path_list[:-1]))
+            
+            try:
+                os.makedirs(local_file_path)
+            except OSError as e:
+                pass
+
+            local_filename = os.path.join(local_file_path, cloud_path_list[-1])
+            print 'downloading to bucket {0}'.format(select_bucket)
+            print 'downloading from filename {0}'.format(cloud_filename)
+            print 'downloading to {0}'.format(local_filename)
+            self.client.download_file(select_bucket, cloud_filename, 
+                local_filename)
+
+    def delete_files(self, select_files, select_bucket):
+        # incomplete !!!!!
+        # delete objects
+        objects_delete = [{'Key': filename} for filename in select_files]
+        
+        response = client.delete_objects(Bucket=select_bucket, 
+            Delete={'Objects': objects_delete})
+
