fc5a9bbdb6ef2b8f5db3bf07a94317ac430c0f7a
quiota-leightondong <leighton.dong@quiota.com>
2016-09-15 17:20:49 -0700

    edit example

--- example.py
+++ example.py
@@ -12,32 +12,46 @@ s3.creat_client()
 
 select_bucket = 'test.tf.quiota.com'
 
+
+###############################################################################
 # get filenames
 s3.get_filenames(select_bucket)
 
 # print filenames
 for filename in s3.filenames:
 	print filename
+###############################################################################
 
+
+###############################################################################
 # upload single file
 local_filename = './test.txt'
 cloud_filename = 'test/test/test.txt'
 
 s3.upload_single_file(local_filename, select_bucket, cloud_filename, 
     cloud_storage_class="STANDARD")
+###############################################################################
 
+
+###############################################################################
 # download single file2
 local_filename = './test/test1/test.txt'
 cloud_filename = 'test/test/test.txt'
 s3.download_single_file(local_filename, select_bucket, cloud_filename)
+###############################################################################
 
+
+###############################################################################
 # upload multiple files
 local_path = './test/'
 cloud_folder_name = 'test'
 
 s3.upload_multiple_files(local_path, select_bucket, 
     cloud_folder_name, cloud_storage_class="STANDARD")
+###############################################################################
 
+
+###############################################################################
 # download multiple files
 s3.get_filenames(select_bucket)
 conditions = ['/test1/']
@@ -45,3 +59,4 @@ select_files = [f for f in s3.filenames if any(condition in f for condition in c
 
 local_path = './'
 s3.download_multiple_files(select_files, local_path, select_bucket)
+###############################################################################
